d23bbe44ca38f52ba00961758d7e53e151b1316a
JumpiX <90917318+JumpiX64@users.noreply.github.com>
2026-05-26 20:43:04 +0200

    Update game_logic

--- src/game_logic.c
+++ src/game_logic.c
@@ -2,31 +2,31 @@
 
 void game_init(GameData *g)
 {
-    g->score_p1        = 0;
-    g->score_p2        = 0;
-    g->wall_hits       = 0;
+    g->score_p1 = 0;
+    g->score_p2 = 0;
+    g->wall_hits = 0;
 
-    g->title_ball_x     = TITLE_BALL_X_START;
-    g->title_ball_y     = TITLE_BALL_Y_START;
+    g->title_ball_x = TITLE_BALL_X_START;
+    g->title_ball_y = TITLE_BALL_Y_START;
     g->title_ball_speed = TITLE_BALL_SPEED_START;
-    g->title_top_pause  = 0;
+    g->title_top_pause = 0;
 
     game_reset_ball(g, 1);
 }
 
 void game_reset_ball(GameData *g, int direction)
 {
-    g->pong1_y     = PADDLE_Y_START;
-    g->pong2_y     = PADDLE_Y_START;
+    g->pong1_y = PADDLE_Y_START;
+    g->pong2_y = PADDLE_Y_START;
     g->ball_game_x = BALL_START_X;
     g->ball_game_y = BALL_START_Y;
-    g->ball_dx     = (direction >= 0) ? BALL_SPEED_INITIAL : -BALL_SPEED_INITIAL;
-    g->ball_dy     = 0.0f;
+    g->ball_dx = (direction >= 0) ? BALL_SPEED_INITIAL : -BALL_SPEED_INITIAL;
+    g->ball_dy = 0.0f;
 }
 
 void game_update_paddles(GameData *g, const InputState *input, GameState state)
 {
-    if (input->p1_joy_y >  JOYSTICK_DEADZONE && g->pong1_y > PADDLE_Y_MIN) g->pong1_y -= PADDLE_STEP;
+    if (input->p1_joy_y > JOYSTICK_DEADZONE && g->pong1_y > PADDLE_Y_MIN) g->pong1_y -= PADDLE_STEP;
     if (input->p1_joy_y < -JOYSTICK_DEADZONE && g->pong1_y < PADDLE_Y_MAX) g->pong1_y += PADDLE_STEP;
 
     if (state == STATE_GAME) {
@@ -62,23 +62,23 @@ void game_check_wall_collision(GameData *g)
 
 int game_check_paddle_collision(GameData *g)
 {
-    if (g->ball_game_x <= BALL_LEFT_BOUND                       &&
-        g->ball_game_y + BALL_SPRITE_HALF >= g->pong1_y         &&
-        g->ball_game_y                    <= g->pong1_y + PADDLE_HEIGHT)
+    if (g->ball_game_x <= BALL_LEFT_BOUND &&
+        g->ball_game_y + BALL_SPRITE_HALF >= g->pong1_y &&
+        g->ball_game_y <= g->pong1_y + PADDLE_HEIGHT)
     {
         g->ball_game_x = BALL_LEFT_BOUND;
-        g->ball_dx     = -g->ball_dx;
-        g->ball_dy    += (g->ball_game_y - (g->pong1_y + PADDLE_CENTER_OFFSET)) / BALL_ANGLE_DIVISOR;
+        g->ball_dx = -g->ball_dx;
+        g->ball_dy += (g->ball_game_y - (g->pong1_y + PADDLE_CENTER_OFFSET)) / BALL_ANGLE_DIVISOR;
         return 1;
     }
 
-    if (g->ball_game_x >= BALL_RIGHT_BOUND                      &&
-        g->ball_game_y + BALL_SPRITE_HALF >= g->pong2_y         &&
-        g->ball_game_y                    <= g->pong2_y + PADDLE_HEIGHT)
+    if (g->ball_game_x >= BALL_RIGHT_BOUND &&
+        g->ball_game_y + BALL_SPRITE_HALF >= g->pong2_y &&
+        g->ball_game_y <= g->pong2_y + PADDLE_HEIGHT)
     {
         g->ball_game_x = BALL_RIGHT_BOUND;
-        g->ball_dx     = -g->ball_dx;
-        g->ball_dy    += (g->ball_game_y - (g->pong2_y + PADDLE_CENTER_OFFSET)) / BALL_ANGLE_DIVISOR;
+        g->ball_dx = -g->ball_dx;
+        g->ball_dy += (g->ball_game_y - (g->pong2_y + PADDLE_CENTER_OFFSET)) / BALL_ANGLE_DIVISOR;
         return 2;
     }
 
@@ -88,8 +88,8 @@ int game_check_paddle_collision(GameData *g)
 void game_apply_speed_boost(GameData *g)
 {
     if (g->wall_hits >= WALL_HIT_TRIGGER) {
-        g->ball_dx  += (g->ball_dx > 0) ? WALL_HIT_BOOST : -WALL_HIT_BOOST;
-        g->ball_dy  += (g->ball_dy > 0) ? WALL_HIT_BOOST : -WALL_HIT_BOOST;
+        g->ball_dx += (g->ball_dx > 0) ? WALL_HIT_BOOST : -WALL_HIT_BOOST;
+        g->ball_dy += (g->ball_dy > 0) ? WALL_HIT_BOOST : -WALL_HIT_BOOST;
         g->wall_hits = 0;
     }
 }
@@ -123,16 +123,16 @@ void game_update_title_ball(GameData *g)
         g->title_top_pause--;
     } else {
         g->title_ball_speed += BALL_GRAVITY;
-        g->title_ball_y     += g->title_ball_speed;
+        g->title_ball_y += g->title_ball_speed;
     }
 
     if (g->title_ball_y >= BALL_GROUND_Y) {
-        g->title_ball_y     = BALL_GROUND_Y;
+        g->title_ball_y = BALL_GROUND_Y;
         g->title_ball_speed = -g->title_ball_speed * BALL_BOUNCE_FACTOR;
     }
 
     if (g->title_ball_y <= BALL_TOP_Y) {
-        g->title_ball_y     = BALL_TOP_Y;
+        g->title_ball_y = BALL_TOP_Y;
         g->title_ball_speed = 0;
         g->title_top_pause  = BALL_TOP_PAUSE;
     }
